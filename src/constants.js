023b91c7c72f69446ea1d65976bc8abc03801200
Rahul <1rahul.1verma@gmail.com>
2021-08-16 18:09:43 +0530

    deploy

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,12 @@
-const ROOT_URL = "http://localhost:3131/";
+const ROOT_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3131/"
+    : "https://chat-server-dummy-app.herokuapp.com/";
 const MESSAGE_FILE =
   "/Users/rahul/Desktop/Practice/dummy-chat/src/Server/message.json";
 const USER_FILE =
-    "/Users/rahul/Desktop/Practice/dummy-chat/src/Server/user.json";
-const CHAT_FILE = "/Users/rahul/Desktop/Practice/dummy-chat/src/Server/chat.json";
+  "/Users/rahul/Desktop/Practice/dummy-chat/src/Server/user.json";
+const CHAT_FILE =
+  "/Users/rahul/Desktop/Practice/dummy-chat/src/Server/chat.json";
 const BASE_URL = "http://localhost:3001";
-module.exports = { ROOT_URL, MESSAGE_FILE, USER_FILE, CHAT_FILE ,BASE_URL };
+module.exports = { ROOT_URL, MESSAGE_FILE, USER_FILE, CHAT_FILE, BASE_URL };
